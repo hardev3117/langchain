64f634c63d4fad19501e243a445a7881233800fd
hardev3117@gmail.com <hardev3117@gmail.com>
2025-07-10 11:23:07 +0530

    added chains and parsers
    added chains and parsers

---
+++ structuredoutputparser.py
@@ -0,0 +1,38 @@
+#from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+
+load_dotenv()
+
+# # Define the model
+# llm = HuggingFaceEndpoint(
+#     repo_id="google/gemma-2-2b-it",
+#     task="text-generation"
+# )
+
+# model = ChatHuggingFace(llm=llm)
+
+model=ChatOpenAI()
+
+schema = [
+    ResponseSchema(name='fact_1', description='Fact 1 about the topic'),
+    ResponseSchema(name='fact_2', description='Fact 2 about the topic'),
+    ResponseSchema(name='fact_3', description='Fact 3 about the topic'),
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template = PromptTemplate(
+    template='Give 3 fact about {topic} \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+#print(template.invoke({'topic':'black hole'}))
+
+chain = template | model | parser
+
+result = chain.invoke({'topic':'black hole'})
+
+print(result)
